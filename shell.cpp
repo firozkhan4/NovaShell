af80d776609dd952791b2f925dca7269ac405180
fiorzkhan4 <firozkhan907979@gmail.com>
2024-09-21 13:53:07 +0530

    new commit

--- shell.cpp
+++ shell.cpp
@@ -1,6 +1,5 @@
 #include "shell.h"
 #include "file.h"
-#include <thread>
 
 FileSystem fs("/");
 
--- shell.h
+++ shell.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <thread>
 #include <vector>
 using namespace std;
 
